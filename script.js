03ddeb8f9a9fd1287010e1b1d23e77945edf9ead
thaissaaa <111879719+thaissaaa@users.noreply.github.com>
2024-05-27 21:55:30 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResultado = document.querySelector(".texto-resultado");
+ const caixaPrincipal = document.querySelector(".caixa-principal");
+ const caixaPerguntas = document.querySelector(".caixa-perguntas");
+ const caixaAlternativas = document.querySelector(".caixa-alternativas");
+ const caixaResultado = document.querySelector(".caixa-resultado");
+ const textoResultado = document.querySelector(".texto-resultado");
 
-const perguntas = [
+  const perguntas = [
     {
         enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
         alternativas: [
